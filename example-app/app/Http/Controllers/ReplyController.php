ecb725e06f2ede5d5b6a8b3dc60e115c2a71215b
SonTT119 <sonden02092002@gmail.com>
2023-12-19 23:49:42 +0700

    reply

--- example-app/app/Http/Controllers/ReplyController.php
+++ example-app/app/Http/Controllers/ReplyController.php
@@ -4,8 +4,150 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Reply;
 
 class ReplyController extends Controller
 {
-    //
+    // public function index()
+    // {
+    //     $replies = Reply::all();
+    //     return view('replies.index', ['replies' => $replies]);
+    // }
+
+    // public function createR(Request $request)
+    // {
+    //     $data = $request->validate([
+    //         'content' => 'required',
+            
+    //         'book_review_id' => 'required|numeric',
+    //         'user_id' => 'required|numeric',
+    //     ]);
+    //     $newReply = Reply::create($data);
+    //     $bookReview = $newReply->bookReview;
+    //     $user = $newReply->user;
+
+    //     if ($user === null) {
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'No user found',
+    //         ], 404);
+    //     }
+
+    //     if ($bookReview === null) {
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'No book review found',
+    //         ], 404);
+    //     }
+
+    //     return response()->json([
+    //         'success' => true,
+    //         'data' => $newReply,
+    //     ], 200);
+
+    //     // return view('replies.create');
+    // }
+
+    // public function 
+
+    // public function update(Request $request, Reply $reply)
+    // {
+    //     $data = $request->validate([
+    //         'content' => 'required',
+            
+    //         'book_review_id' => 'required|numeric',
+    //         'user_id' => 'required|numeric',
+    //     ]);
+    //     $reply->update($data);
+    //     // return redirect(route('reply.index'))->with('success', 'Reply updated successfully');
+    // }
+
+    // public function destroy(Reply $reply)
+    // {
+    //     $reply->delete();
+    //     // return redirect(route('reply.index'))->with('success', 'Reply deleted successfully');
+    // }
+
+    public function getAllReplies()
+    {
+        $replies = Reply::all();
+        // return view('replies.index', ['replies' => $replies]);
+        if ($replies->isEmpty()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No replies found',
+            ], 404);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $replies,
+        ], 200);
+    }
+    
+    public function getAllRepliesForReview($id)
+    {
+        $replies = Reply::where('book_review_id', $id)->get();
+        if ($replies->isEmpty()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No replies found',
+            ], 404);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $replies,
+        ], 200);
+    }
+
+    public function createReply(Request $request)
+    {
+        $data = $request->validate([
+            'content' => 'required',
+            
+            'book_review_id' => 'required|numeric',
+            'user_id' => 'required|numeric',
+        ]);
+        $newReply = Reply::create($data);
+        $bookReview = $newReply->bookReview;
+        $user = $newReply->user;
+
+        if ($user === null) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No user found',
+            ], 404);
+        }
+
+        if ($bookReview === null) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No book review found',
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $newReply,
+        ], 200);
+
+        // return view('replies.create');
+    }
+
+    public function updateReply(Request $request, Reply $reply)
+    {
+        $data = $request->validate([
+            'content' => 'required',
+            
+            'book_review_id' => 'required|numeric',
+            'user_id' => 'required|numeric',
+        ]);
+        $reply->update($data);
+        // return redirect(route('reply.index'))->with('success', 'Reply updated successfully');
+    }
+
+    public function deleteReply(Reply $reply)
+    {
+        $reply->delete();
+        // return redirect(route('reply.index'))->with('success', 'Reply deleted successfully');
+    }
 }
--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookReviewController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ReplyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,3 +31,14 @@ Route::put('/user/update', [UserController::class, 'update']);
 //auth:sanctum yeu cau Authorization header khi gui request
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->get('/home', [UserController::class, 'show']);
+
+Route::get('/reply', [ReplyController::class, 'getAllReplies']);
+Route::post('/reply', [ReplyController::class, 'createReply']);
+Route::get('/reply/{reply}', [ReplyController::class, 'getAllRepliesForReview']);
+Route::put('/reply/{reply}/update', [ReplyController::class, 'updateReply']);
+Route::delete('/reply/{reply}/destroy', [ReplyController::class, 'deleteReply']);
+
+// Route::middleware('auth:sanctum')->get('/reply/{reply}', [ReplyController::class, 'getAllRepliesForReview']);
+// Route::middleware('auth:sanctum')->put('/reply/{reply}/update', [ReplyController::class, 'updateReply']);
+// Route::middleware('auth:sanctum')->delete('/reply/{reply}/destroy', [ReplyController::class, 'deleteReply']);
+
